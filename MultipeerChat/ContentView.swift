2c07632baa4ffcab05b7e55417e70247f07e3974
Surasak W <s.wattanapradit@gmail.com>
2021-01-20 09:52:10 +0700

    change init project with swiftUI

---
+++ MultipeerChat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MultipeerChat
+//
+//  Created by darklightsak_office on 20/1/2564 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
